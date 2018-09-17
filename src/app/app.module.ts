1bf6a12958a27acb8a22459141cff287db36310a
Juan Mora <raicerk@gmail.com>
2018-09-16 22:06:28 -0300

    Crud funcional

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,18 @@ import { TabsPage } from '../pages/tabs/tabs';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
+import * as firebase from 'firebase';
+ 
+const config = {
+  apiKey: "AIzaSyDfufyZSQXEDy6XKcLwDyAtuHs5WvBgT4I",
+  authDomain: "smartfit-16f6c.firebaseapp.com",
+  databaseURL: "https://smartfit-16f6c.firebaseio.com",
+  projectId: "smartfit-16f6c",
+  storageBucket: "smartfit-16f6c.appspot.com",
+  messagingSenderId: "526276590283"
+};
+firebase.initializeApp(config);
+
 @NgModule({
   declarations: [
     MyApp,
